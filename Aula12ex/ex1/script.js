b9d2cbe0ad1b4601c0e48c5be0fda07e68bf14f6
Gabriel Monteiro <gabrielnotavel@gmail.com>
2024-04-11 10:57:16 -0300

    Condicional

---
+++ Aula12ex/ex1/script.js
@@ -0,0 +1,19 @@
+function carregar(){
+    var msg = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var hora = new Date().getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 0 && hora < 12){
+        //Bom dia
+        img.src = "/Aula12ex/fotos/ftmanha.jpg"
+        document.body.style.background = '#D9CCC5'
+    } else if (hora >= 12 && hora <= 18){
+        //Boa tarde
+        img.src = "/Aula12ex/fotos/fttarde.jpg"
+        document.body.style.background = '#F29E6D'
+    }else{
+        //Boa noite
+        img.src = "/Aula12ex/fotos/ftnoite.jpg"
+        document.body.style.background = '#0B0B0D'
+    }
+}
